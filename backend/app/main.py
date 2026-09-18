652d09e5c10ac8faa9fa11ce66d7e1ee175c2209
sijanbasnet <sijan.basnet@8squarei.com>
2026-09-18 15:38:58 +0545

    db required change

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-
+from sqlalchemy import text
 from app.api.router import api_router
 from app.core.config import get_settings
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from app.data import SessionLocal
+from app.model import Task as TaskModel
 
 settings = get_settings()
 
@@ -24,7 +28,53 @@ app.add_middleware(
 app.include_router(api_router, prefix=settings.api_prefix)
 
 
-@app.get("/", include_in_schema=False)
-def root() -> dict[str, str]:
-    return {"message": settings.app_name, "docs": "/docs"}
+class TaskCreate(BaseModel):
+    title: str
+    description: str
+    isCompleted: bool = False
 
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def root():
+    return {"message": "API is working correctly"}
+
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(TaskModel).all()
+
+
+@app.post("/tasks")
+def create_task(
+    task: TaskCreate,
+    db: Session = Depends(get_db)
+):
+    new_task = TaskModel(
+        title=task.title,
+        description=task.description,
+        isCompleted = task.isCompleted
+        
+    )
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
+    return {
+        "message": "Task created",
+        "task": new_task
+    }
+
+@app.get("/healthdb")
+def health(db: Session = Depends(get_db)):
+    db.execute(text("SELECT 1"))
+    return {"status": "connected"}
